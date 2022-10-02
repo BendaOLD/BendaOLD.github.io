5c486e472f0f71c1248072fe6042d9b675243260
snakeout96 <86480748+snakeout96@users.noreply.github.com>
2022-10-02 20:09:58 +0200

    update

--- script.js
+++ script.js
@@ -1,21 +1,65 @@
 const states = [
-    ["Hosting *thanks daniel <3*", 'public', state(1)],
-    ["Database", 'database', state(1)],
-    ["Register System", 'account_circle', state(4)],
-    ["Login system", 'login', state(4)],
-    ["Comment system", 'chat', state(1)],
-    ["GET/POST", 'swap_horiz', state(1)],
-    ["Server", 'dns', state(1)],
-    ["Uploading System",'file_upload', state(1)],
-    ["Link/Fetching System", 'link', state(1)],
-    ["Responsive", 'smartphone', state(7)],
-    ["Front-End", 'palette', state(3)]
-]
+    {
+        "system": "Hosting",
+        "icon": 'public',
+        "state": state(1)
+    },
+    {
+        "system": "Database",
+        "icon": 'database',
+        "state": state(1)
+    },
+    {
+        "system": "Register System",
+        "icon": 'account_circle',
+        "state": state(2)
+    },
+    {
+        "system": "Login System",
+        "icon": 'login',
+        "state": state(2)
+    },
+    {
+        "system": "Comment System",
+        "icon": 'chat',
+        "state": state(2)
+    },
+    {
+        "system": "GET/POST",
+        "icon": 'swap_horiz',
+        "state": state(1)
+    },
+    {
+        "system": "Server",
+        "icon": 'dns',
+        "state": state(1)
+    },
+    {
+        "system": "Uploading System",
+        "icon": 'file_upload',
+        "state": state(6)
+    },
+    {
+        "system": "Link/Fetching System",
+        "icon": 'link',
+        "state": state(1)
+    },
+    {
+        "system": "Front-End",
+        "icon": 'palette',
+        "state": state(5)
+    },
+    {
+        "system": "Responsive",
+        "icon": 'smartphone',
+        "state": state(7)
+    }
+    ]
 
 function state(number) {
     switch (number) {
         case 1: return ["Operational", "led1"] //Full system works well without any issues.
-        case 2: return ["Slow/Need Performance", "led2"] //System doesn't work at 100% of his power, not a big problem.
+        case 2: return ["Need Performance", "led2"] //System doesn't work at 100% of his power, not a big problem.
         case 3: return ["Degraded Performance", "led3"] //System meets a new problem.
         case 4: return ["Updating", "led4"] //System has turned off to upload a new feature.
         case 5: return ["In Mantenaince", "led5"] //System has turned off to fix a bug.
@@ -30,13 +74,13 @@ states.forEach(container => {
     section.innerHTML += 
    `<div class="container">
         <div>
-            <h2>${container[0]}</h2>
+            <h2>${container.system}</h2>
             <p></p>
-            <span class="material-symbols-outlined">${container[1]}</span>
+            <span class="material-symbols-outlined">${container.icon}</span>
         </div>
-        <div>
-            <p>${container[2][0]}</p>
-            <div class="${container[2][1]}"></div>
+        <div class="state">
+            <p>${container.state[0]}</p>
+            <div class="${container.state[1]} led"></div>
         </div>
     </div>`
 });
